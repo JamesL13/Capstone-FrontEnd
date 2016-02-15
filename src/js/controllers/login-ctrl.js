ccf7ec3c89cf93da8972b77b9fb03415ffe04620
Devin Clark <devin.clark27@gmail.com>
2016-02-15 17:42:34 -0600

    Finishing login / create account

--- src/js/controllers/login-ctrl.js
+++ src/js/controllers/login-ctrl.js
@@ -7,24 +7,31 @@ angular.module('Songs').controller('LoginCtrl', ['$scope', '$http', '$cookieStor
 function LoginCtrl($scope, $http, $cookieStore) {
     var server = 'http://thomasscully.com';
     $scope.formErrors = false;
+    $scope.formErrorMessage = "";
 
     $scope.submit = function(isValid) {
         if (isValid) {
-            $http.post(server + '/accounts/login', $scope.loginForm).then(successCallback, errorCallback);
+            $http.post(server + '/accounts/login', $scope.loginDetails).then(successCallback, errorCallback);
         } else {
             $scope.formErrors = true;
+            $scope.formErrorMessage = "Invalid username / password.";
         }
     }
 
     var successCallback = function(response) {
         
-        console.log(response);
-        $cookieStore.put('isLoggedIn', true);
-        window.location = "#/host";
-        location.reload();
-    }
+        if (response.data) {
+            $cookieStore.put('isLoggedIn', true);
+            window.location = "#/host";
+            location.reload();
+        } else {
+            $scope.formErrorMessage = "Invalid username / password combination.";
+            $scope.formErrors = true;
+        }
+    }   
     var errorCallback = function(response) {
-        $scope.formErrorMessage = response.data;
+        $scope.formErrorMessage = "Server not responding. Please try again later.";
+        $scope.formErrors = true;
 
     }
     var init = function() {
