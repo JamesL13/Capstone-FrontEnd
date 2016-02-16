e5ef80fef50b64f62ea35a70ca489f4aa1b11276
Devin Clark <devin.clark27@gmail.com>
2016-02-16 10:18:39 -0600

    Adding JSMediaTags and example implementation

--- src/js/controllers/host-ctrl.js
+++ src/js/controllers/host-ctrl.js
@@ -14,4 +14,22 @@ function HostCtrl($scope, $http, $cookieStore) {
     $scope.manageJukebox = function() {
         window.location = "#/managejukebox";
     }
+
+
+    var jsmediatags = window.jsmediatags; // From remote host
+    var inputTypeFile = document.querySelector('input[type="file"]');
+
+    inputTypeFile.addEventListener("change", function (event) {
+        var file = event.target.files[0];
+        jsmediatags.read(file, {
+            onSuccess: function (tag) {
+                console.log(tag);
+            },
+            onError: function (error) {
+                console.log(error);
+            }
+        });
+    }, false);
+
+
 }
