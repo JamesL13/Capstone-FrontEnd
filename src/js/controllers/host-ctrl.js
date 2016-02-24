59c564b9e428ceeab6c36845f591d672a0f2e291
Devin Clark <devin.clark27@gmail.com>
2016-02-24 17:57:47 -0600

    Adding playlist creation / Deletion per account

--- src/js/controllers/host-ctrl.js
+++ src/js/controllers/host-ctrl.js
@@ -6,6 +6,9 @@ angular.module('Songs').controller('HostCtrl', ['$scope', '$http', '$cookieStore
 
 function HostCtrl($scope, $http, $cookieStore) {
     var server = 'https://thomasscully.com';
+    $scope.formErrors = false;
+    $scope.formErrorMessage = "";
+    $scope.userId = $cookieStore.get('userId');
 
     $scope.manageAccount = function() {
         window.location = "#/manageaccount";
@@ -14,11 +17,60 @@ function HostCtrl($scope, $http, $cookieStore) {
     $scope.manageJukebox = function() {
         window.location = "#/managejukebox";
     }
+    $scope.deletePlaylist = function() {
+        var r = confirm("Are you sure you'd like to delete this playlist?");
+        if (r == true) {
+            $http.delete(server + "/playlists?id=" + $scope.playlist_id).then(
+                function(response) {
+                    location.reload();
+                },
+                function(response) {
+                    console.log("Server did not successfully complete request.");
+                }
+            );
+        }
+    }
+    $scope.submit = function(isValid) {
+        if (isValid) {
+            var data = {
+                "user__account": $scope.userId,
+                "playlist_name": $scope.loginDetails.playlist_name,
+                "description": $scope.loginDetails.description,
+                "password": $scope.loginDetails.password
+            };
+            $http.post(server + '/playlists', data).then(
+                function(response) {
+                    location.reload();
+                },
+                function(response) {
+                    console.log("Server did not successfully complete request.");
+                }
+            );
+        } else {
+            $scope.formErrors = true;
+            $scope.formErrorMessage = "There was a problem with your input. Please try again.";
+        }
+    }
+
+    // Playlist callback
+    var playlistResponse = function(response) {
+        if (response.data == '') {
+            $scope.hasPlaylist = false;
+        } else {
+            $scope.hasPlaylist = true;
+            $scope.playlist_id = response.data[0].id;
+            $scope.playlist_name = response.data[0].playlist_name;
+            $scope.description = response.data[0].description;
+            $scope.password = response.data[0].password;
+        }
+    }
+    var errorCallback = function(response) {}
 
     var init = function() {
         if (!$cookieStore.get('isLoggedIn')) {
             window.location = "#/login";
         }
+        $http.get(server + '/playlists?account__id=' + $cookieStore.get('userId')).then(playlistResponse, errorCallback);
     }
 
     init();
