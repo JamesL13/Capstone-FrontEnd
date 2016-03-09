6494696cc34b5f099b25944a4e2f1692617a6353
James Landy <jmlmdf@mail.missouri.edu>
2016-03-08 21:29:17 -0600

    Modal pops up and code is there for connecting to jukebox but submitting form does not work

--- src/js/controllers/findhost-ctrl.js
+++ src/js/controllers/findhost-ctrl.js
@@ -2,39 +2,70 @@
  * Find Host Controller
  */
 
-angular.module('Songs').controller('FindHostCtrl', ['$scope','$http', '$cookieStore', FindHostCtrl]);
+angular.module('Songs').controller('FindHostCtrl', ['$scope','$http', '$uibModal', '$cookieStore', FindHostCtrl]);
 
-function FindHostCtrl($scope, $http) {
+function FindHostCtrl($scope, $http, $uibModal) {
     var server = 'https://thomasscully.com';
+    $scope.modalInstance = null;
+    $scope.animationsEnabled = true;
+    $scope.formErrors = false;
+    $scope.formErrorMessage = "";
+    $scope.currentId = null;
 
-    $http.defaults.headers.common = {'secret-token': 'aBcDeFgHiJkReturnOfTheSixToken666666',
+    $http.defaults.headers.common = {
+        'secret-token': 'aBcDeFgHiJkReturnOfTheSixToken666666',
         'Accept': "application/json, text/plain, */*"
     };
 
-    /*var config = {headers:  {
-        "secret-token" : "aBcDeFgHiJkReturnOfTheSixToken666666"
-    }
-    };*/
-
     $scope.jukeboxes = [];
 
-    var successCallback = function(response) {
-        console.log(response);
-        //window.location = "#/host";
-        //location.reload();
+    var successCallback = function (response) {
+        //console.log(response);
+        if (response == true) {
+            $cookieStore.put('isConnected', true);
+            $cookieStore.put('jukeBoxid', $scope.currentId);
+            if ($scope.modalInstance != null) {
+                $scope.modalInstance.$dismiss(connect);
+            }
+        } else {
+            $scope.formErrorMessage = "Invalid password.";
+            $scope.formErrors = true;
+        }
+        if ($scope.modalInstance != null) {
+            $scope.modalInstance.$dismiss(connect);
+        }
     }
 
-    var errorCallback = function(response) {
+    var errorCallback = function (response) {
         console.log(response);
     }
 
-    var init = function() {
-        console.log("submitting");
-        $http.get(server + '/playlists').success(function(jukebox) {
+    var init = function () {
+        $http.get(server + '/playlists').success(function (jukebox) {
             $scope.jukeboxes = jukebox;
         }).then(successCallback, errorCallback);
     }
 
+    $scope.open = function (size, id) {
+        console.log(id);
+        $scope.currentId = id;
+        $scope.modalInstance = $uibModal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'templates/modalContent.html',
+            size: size,
+        });
+    };
+
+    $scope.submit = function(isValid) {
+        console.log("Here");
+        if (isValid) {
+            $http.post(server + '/playlists/join', {'id': $scope.currentId}, $scope.connectDetails).then(successCallback, errorCallback);
+        } else {
+            $scope.formErrors = true;
+            $scope.formErrorMessage = "Invalid password.";
+        }
+    };
+
     init();
-  
+
 }
