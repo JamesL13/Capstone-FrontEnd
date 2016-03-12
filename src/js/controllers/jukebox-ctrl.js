6de13dda247aa446b9adad7b522bee1ef42bf334
Devin Clark <devin.clark27@gmail.com>
2016-03-12 14:04:44 -0600

    Add host authentication

---
+++ src/js/controllers/jukebox-ctrl.js
@@ -0,0 +1,22 @@
+/**
+ * Jukebox Controller
+ */
+
+angular.module('Songs').controller('JukeboxCtrl', ['$scope', '$http', '$cookieStore', JukeboxCtrl]);
+
+function JukeboxCtrl($scope, $http, $cookieStore) {
+    var server = 'https://thomasscully.com';
+    $http.defaults.headers.common = {
+        'secret-token': 'aBcDeFgHiJkReturnOfTheSixToken666666',
+        'Accept': "application/json, text/plain, */*"
+    };
+
+
+    var init = function() {
+        if ($cookieStore.get('isConnectedToPlaylist') == undefined || !$cookieStore.get('isConnectedToPlaylist')) {
+            window.location = "#/findhost"
+        }
+
+    }
+    init();
+}
